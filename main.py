67665d1a927f047d90d22e4d29814b764b1d9932
PhilGW <philip.g.wagner@gmail.com>
2021-10-13 15:39:21 -0600

    A full working version that opens up a single Websockets connection, and fires off a new message (timestamp) to the server every 5 seconds

--- main.py
+++ main.py
@@ -20,22 +20,7 @@ print("imports successful...")
 # main() is the main (parent) thread, which starts the other thread(s)
 # and maintains a socketio client to handle communication to/from the Flask server
 async def main():
-    # Set up queues to be shared by threads:
-    q_changes = queue.Queue()  # queue to track requests from the browser
-    q_outbox = queue.Queue()  # queue to track messages that should be sent to browser
-
-    # The data_log dictionary specifies all of the variables that
-    # will be logged and plotted. The keys are the database columns,
-    # and the items are the SQL data types.
-    data_log = {
-        'timestamp': 'DATETIME',
-        'elapsed_min': 'NUMERIC',
-        'expt_id': 'TEXT',
-        'mass1': 'NUMERIC',
-        'sinedata': 'NUMERIC',
-        'cosinedata': 'NUMERIC'
-    }
-    data_table_name = 'MyDataLog'  # The name of the table in the database
+# def main():
 
     # The device_params dictionary stores the current state of all device parameters/settings
     # This should include default settings, start values, PID settings, etc.)
@@ -54,11 +39,6 @@ async def main():
         'green_led_pin': 17,
     }
 
-    global ws_connected  # Flag to indicate whether socketio connection is active
-    global dev_id, this_expt_id  # experiment id for the current experiment on this device
-    sio_connected = False  # Intially, controller is not yet connected to socketio server
-    this_device_id = device_params['dev_id']
-    this_expt_id = device_params['expt_id']
     # Define a thread that controls the device:
     # #device_thread = threading.Thread(target=run_device,
     #                                  args=(1, q_changes, q_outbox, device_params, data_log, data_table_name)
@@ -67,15 +47,23 @@ async def main():
     #https://stackoverflow.com/questions/62177517/best-way-to-share-data-with-a-producer-coroutine-loop-from-the-python-websockets
 
     #asyncio.get_event_loop().run_until_complete(hello())
-
-
+    #await hello()
+    # q_outbox = asyncio.Queue()
     uri = "ws://127.0.0.1:8000/ws/devices/1"
-    async with websockets.connect(uri) as websocket:
+    # async with websockets.connect(uri) as websocket:
+    #     while True:
+    #         msg = await(run_device())
+    #         await websocket.send(msg)
+    async with websockets.connect(uri) as ws:
         while True:
-            msg = await(run_device())
-            await websocket.send(msg)
-
-
+            await asyncio.sleep(5)
+            current_timestamp = datetime.now()
+            print("Sending the current time: ", current_timestamp)
+            await ws.send(str(current_timestamp))
+            print("message sent!")
+            await asyncio.sleep(3)
+            response_msg = await ws.recv()
+            print("message received!!", response_msg)
 
 # async def producer_handler():
 #     uri = "ws://127.0.0.1:8000/ws/devices/1"
@@ -94,7 +82,7 @@ async def send_stamp():
 
 async def hello():
     uri = "ws://127.0.0.1:8000/ws/devices/1"
-    async with websockets.connect(uri) as websocket:
+    async with websockets.connect(uri) as websocket: #Connection/handshake occurs here
         name = input("What's your name? ")
 
         await websocket.send(name)
@@ -115,6 +103,7 @@ async def run_device(): #thread_id, q_changes, q_out, dparams, datalog, data_tab
 
 
 if __name__ == "__main__":
+    # main()
     asyncio.run(main())
 
 
