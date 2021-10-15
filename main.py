2a5e10deea54e321d83e238f6403447c45e30865
PhilGW <philip.g.wagner@gmail.com>
2021-10-15 10:48:05 -0600

    New class-based system works! Sends and receives simultaneously.

--- main.py
+++ main.py
@@ -64,38 +64,16 @@ async def main():
 
     #pdb.set_trace()
     my_controller = DeviceController(ws, 1)
-    data_transmission_task = asyncio.create_task( transmit_data_periodically(ws) )
-    listen_task = asyncio.create_task( listen_for_instructions(ws) )
+    #data_transmission_task = asyncio.create_task( transmit_data_periodically(ws) )
+    #data_transmission_task = asyncio.create_task(my_controller.run_controller(ws))
+    data_transmission_task = my_controller.get_asycio_task_for_controller()
+    listen_task = asyncio.create_task( listen_for_instructions(ws, my_controller) )
     # data_transmission_task keeps sending out data every 5 seconds forever
     await data_transmission_task
-
-    while True:
-        # print("listening for instructions...")
-        # await asyncio.sleep(3)
-        new_msg = await listen_task
-        print("message received in main(): ", new_msg)
-
-    # try:
-    #     loop.run_until_complete(task)
-    # except asyncio.CancelledError:
-    #     pass
-    # except LostConnectionError:
-    #     pass
-        #code here for when the connection is lost... then it will automatically go back to the top
+    await listen_task
 
     #TODO: Use asyncio.gather to call all of the sensors at once and then send the full message
     # TODO: Write device-io functions so that they use context manager methods (__enter__() and __exit__()) and can therefor be used with 'with'
-    #For import websockets, this works: Open the websocket, then keep it open:
-    # async with websockets.connect(uri) as ws:
-    #     while True:
-    #         await asyncio.sleep(5)
-    #         current_timestamp = datetime.now()
-    #         print("Sending the current time: ", current_timestamp)
-    #         await ws.send(str(current_timestamp))
-    #         print("message sent!")
-    #         await asyncio.sleep(3)
-    #         response_msg = await ws.recv()
-    #         print("message received!!", response_msg)
 
 class DeviceController():
     #Initialization:
@@ -108,24 +86,30 @@ class DeviceController():
         self.device_id += amt
         print("new device_id: ", self.device_id)
 
-async def listen_for_instructions(ws):
-    response_msg = await ws.recv()
-    print("message received in func: ", response_msg)
-    return(response_msg)
+    def get_asycio_task_for_controller(self):
+        return(asyncio.create_task(self.run_controller(self.ws_connection)))
 
+    #Method to run the controller forever, sending out data periodicially:
+    async def run_controller(self, ws):
+        while True:
+            print('Gathering sensor data...')
+            [this_temp, this_pres] = await asyncio.gather(collect_temp_data(), collect_pres_data())
+            current_timestamp = datetime.now()
+            print("Sending current time and data...")
+            new_data = "device_id=" + str(self.device_id) + "  " + str(current_timestamp) + " : " + str(this_temp) + " | " + str(this_pres)
+            await ws.send(new_data)
+            print("Just sent new data: ", new_data)
+            await asyncio.sleep(5)
 
+    # async def get_controller_task(self, ws):
+    #     return(self.)
 
-
-async def transmit_data_periodically(ws):
+async def listen_for_instructions(ws, device):
     while True:
-        print('Gathering sensor data...')
-        [this_temp, this_pres] = await asyncio.gather(collect_temp_data(), collect_pres_data())
-        current_timestamp = datetime.now()
-        print("Sending current time and data...")
-        new_data = str(current_timestamp) + " : " + str(this_temp) + " | " + str(this_pres)
-        await ws.send(new_data)
-        print("Just sent new data: ", new_data)
-        await asyncio.sleep(5)
+        response_msg = await ws.recv()
+        print("message received in func: ", response_msg)
+        device.increase_id(3)
+
 
 # def stop():
 #     task.cancel()
@@ -139,7 +123,6 @@ async def collect_pres_data():
     time.sleep(0.7)
     return(2.1)
 
-
 async def hello():
     uri = "ws://127.0.0.1:8000/ws/devices/1"
     async with websockets.connect(uri) as websocket: #Connection/handshake occurs here
@@ -151,22 +134,17 @@ async def hello():
         greeting = await websocket.recv()
         print(f"Just received> {greeting}")
 
-async def run_device(): #thread_id, q_changes, q_out, dparams, datalog, data_table_name):
-    uri = "ws://127.0.0.1:8000/ws/devices/1"
-    print("New device started!")
-    async with websockets.connect(uri) as websocket:
-        while True:
-            await asyncio.sleep(5)
-            current_timestamp = datetime.now()
-            print("Sending the current time: ", current_timestamp)
-            return(str(current_timestamp))
-
-
 if __name__ == "__main__":
     # main()
     asyncio.run(main())
 
 
-
+# try:
+    #     loop.run_until_complete(task)
+    # except asyncio.CancelledError:
+    #     pass
+    # except LostConnectionError:
+    #     pass
+        #code here for when the connection is lost... then it will automatically go back to the top
 
 
